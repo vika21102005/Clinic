e9d28d6e40d26b3045ae42f8dfb066594f243688
Victoria Konareva <v.v.konarieva@student.khai.edu>
2024-12-10 17:43:19 +0200

    Patient

---
+++ Project A/Patient.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClinicManagement;
+
+namespace Project_A
+{
+    // Клас Patient
+    public class Patient
+    {
+        public string FullName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public List<Appointment> Appointments { get; private set; }
+
+        public Patient(string fullName, DateTime dateOfBirth)
+        {
+            FullName = fullName ?? throw new ArgumentNullException(nameof(fullName));
+            DateOfBirth = dateOfBirth;
+            Appointments = new List<Appointment>();
+        }
+
+        // Асоціація: Планування прийому
+        public void ScheduleAppointment(Clinic clinic, Doctor doctor, Room room, DateTime appointmentDate)
+        {
+            if (clinic == null)
+                throw new ArgumentNullException(nameof(clinic));
+            if (doctor == null)
+                throw new ArgumentNullException(nameof(doctor));
+            if (room == null)
+                throw new ArgumentNullException(nameof(room));
+
+            var appointment = new Appointment(appointmentDate, doctor, this, room);
+            clinic.ScheduleAppointment(appointment);
+        }
+    }
+}
