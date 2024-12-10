a4780cade54f108c18843fcc87e171a9df64446b
Victoria Konareva <v.v.konarieva@student.khai.edu>
2024-12-10 18:00:50 +0200

    Office in the clinic

---
+++ Project A/Room.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_A
+{
+
+    // Клас Room
+    public class Room
+    {
+        public string RoomNumber { get; set; }
+        public string Type { get; set; }
+
+        public Room(string roomNumber, string type)
+        {
+            RoomNumber = roomNumber ?? throw new ArgumentNullException(nameof(roomNumber));
+            Type = type ?? throw new ArgumentNullException(nameof(type));
+        }
+
+        public void ReserveRoom()
+        {
+            Console.WriteLine($"Кабінет {RoomNumber} зарезервовано для прийому.");
+        }
+    }
+
+}
