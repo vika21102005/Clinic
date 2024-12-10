f0d66bbb32f1a7eed7e36ede5647b9c599dfaeeb
Victoria Konareva <v.v.konarieva@student.khai.edu>
2024-12-10 17:55:51 +0200

    Medical service in the clinic

---
+++ Project A/MedicalServisecs.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_A
+{
+    // Клас MedicalService
+    public class MedicalService
+    {
+        public string ServiceName { get; set; }
+        public decimal Price { get; set; }
+
+        public MedicalService(string serviceName, decimal price)
+        {
+            ServiceName = serviceName ?? throw new ArgumentNullException(nameof(serviceName));
+            Price = price;
+        }
+
+        public void ProvideService(Patient patient)
+        {
+            if (patient == null)
+                throw new ArgumentNullException(nameof(patient));
+
+            Console.WriteLine($"Надано медичну послугу '{ServiceName}' пацієнту {patient.FullName}. Вартість: {Price} грн.");
+        }
+    }
+}
